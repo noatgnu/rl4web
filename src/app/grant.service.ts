bef97aac1598252d2f5e5446aa2844793291d365
Toan Phung <toan.phung@uqconnect.edu.au>
2018-12-09 12:06:44 +1000

    update

--- src/app/grant.service.ts
+++ src/app/grant.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Subject} from 'rxjs';
-import {Grant} from "./grant";
+import {Grant} from './grant';
 
 
 @Injectable()
